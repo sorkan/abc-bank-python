c95b7a371a24482dfa982bee5d1d9d198ee650ec
sorkan <sorkan@hotmail.com>
2016-05-25 00:16:15 +0530

    Add files via upload

--- tests/customer_tests.py
+++ tests/customer_tests.py
@@ -18,6 +18,19 @@ def test_statement():
                   "\n\nTotal In All Accounts $3900.00")
 
 
+def test_transfer():
+    checkingAccount = Account(CHECKING)
+    savingsAccount = account(SAVINGS)
+    henry = Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount)
+    checkingAccount.deposit(100.0)
+    savingsAccount.deposit(4000.0)
+    savingsAccount.transferFunds(checkingAccount, 200.0)
+    assert_equals(henry.getStatement(),
+                  "Statement for Henry" +
+                  "\n\nChecking Account\n  deposit $100.00\n  deposit $200.00\nTotal $300.00" +
+                  "\n\nSavings Account\n  deposit $4000.00\n  withdrawal $200.00\nTotal $3800.00" +
+                  "\n\nTotal In All Accounts $4100.00")
+
 def test_oneAccount():
     oscar = Customer("Oscar").openAccount(Account(SAVINGS))
     assert_equals(oscar.numAccs(), 1)
